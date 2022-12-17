97cde0db1b0c253ab774b4f50f4329e503a0fc76
federico <fgc_castro@live.com>
2022-12-17 19:08:02 -0300

    [ADMIN]
    set django administration

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,15 @@ from django.db import models
 class Projects(models.Model):
     name = models.CharField(max_length=200)
 
+    # Extend this class to django admin.
+    def __str__(self) -> str:
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
+    # Extend this class to django admin.
+    def __str__(self) -> str:
+        return self.title + '-' + self.project.name
